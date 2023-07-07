3c29761dbf0fad5d1c54327c367467999534268a
Suven Jagtiani <suvenjagtiani@gmail.com>
2023-07-07 22:42:19 +0530

    update benchmarking shell script

--- translated.sh
+++ translated.sh
@@ -1,14 +1,6 @@
-#!/bin/bash
-
-# Set variables for convenience
-protoFile="/recommendation/proto/recommendation.proto"
-callPath="/recommendation/proto/recommendation_pb2_grpc.RecommendationStub.GetRecommendations"
-targetAddress="127.0.0.1:50051"
-dataPayload='{"require": "dist","lat":37.7867, "lon": -122.4112}'
-
-# Execute the ghz command
 ghz --insecure \
-    --proto $protoFile \
-    --call $callPath \
-    -d $dataPayload \
-    $targetAddress
+--proto=recommendation/proto/recommendation.proto \
+--call=recommendation.Recommendation.GetRecommendations \
+127.0.0.1:50051 \
+-n 5000 \
+-D recommendation/proto/test.json
